4170072d3c822f39bce5247cb69c6b2fb4ef15d6
Noam Green <Noamgr11@gmail.com>
2023-05-10 19:08:30 +0300

    renderList of index

--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -1,3 +1,20 @@
-export function MailList() {
-    return <div>Mail list</div>
+const { Link } = ReactRouterDOM
+// import { MailPreview } from "./book-preview.jsx";
+
+export function MailList({ mails }) {
+    // console.log(mails)
+    // return <ul className="mail-list">
+    //     {mails.map(mail => {
+    //         <li key={mail.id}>
+    //             <article className="mail-preview">
+    //                 <h2>{mail.body}</h2>
+    //                 <h2>{mail.from}</h2>
+
+    //             </article>
+    //         </li>
+
+
+    //     })}
+
+    // </ul>
 }
