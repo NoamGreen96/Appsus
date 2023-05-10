5ce43729ac3d13a92850b301b91ba249a14203ae
Noam Green <Noamgr11@gmail.com>
2023-05-10 12:58:39 +0300

    add long txt

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,13 @@
 // mail service
+// Model - start with a basic model of emails: 
+
+const email = {
+  id: 'e101',
+  subject: 'Miss you!',
+  body: 'Would love to catch up sometimes',
+  isRead: false,
+  sentAt: 1551133930594,
+  removedAt: null,
+  from: 'momo@momo.com',
+  to: 'user@appsus.com'
+}
